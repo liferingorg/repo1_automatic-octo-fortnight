ea1e6a998407cd6e5ea7040ef50d69250e4758c6
Mark Fussell <mark.fussell@emenar.com>
2022-12-06 09:49:40 -0800

    A stub for the secrets

---
+++ soft/app/zoom/lifering_secrets_empty.py
@@ -0,0 +1,19 @@
+headers = {
+    'authorization': "Bearer XXX", # 2023-01-01 01:01
+    'content-type': "application/json"
+}
+
+slack_headers = {
+    'authorization': "Bearer XXX", # 2023-01-01 01:01
+    'content-type': "application/json"
+}
+
+greylist = [
+]
+
+slack_URL = 'https://hooks.slack.com/services/XXX'
+slack_detail_URL = 'https://hooks.slack.com/services/XXX'
+slack_alert_URL = 'https://hooks.slack.com/services/XXX'
+
+#curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' #https://hooks.slack.com/services/XXX
+
